8782253aab032a15d2013193f452409d8628b815
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-05-16 20:56:22 +0200

    Integration of Param Converter

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -31,9 +31,8 @@ class BlogController extends AbstractController
             );
         }
 
-        return $this->render(
-            'blog/index.html.twig',
-            ['articles' => $articles]
+        return $this->render('blog/index.html.twig', [
+                'articles' => $articles]
         );
     }
     /*
@@ -80,15 +79,71 @@ class BlogController extends AbstractController
             );
         }
 
-        return $this->render(
-            'blog/show.html.twig',
-            [
+        return $this->render('blog/show.html.twig', [
                 'article' => $article,
                 'slug' => $slug,
             ]
         );
     }
 
+///*========= Quête 9 Symfony =========
+    /**
+     * Getting a category
+     *
+     * @param Category $category
+     *
+     * @Route("/blog/category/{name}",
+     *     defaults={"category" = null},
+     *     name="show_category")
+     * @return Response A response instance
+     */
+    public function showByCategory(Category $category): response
+    {
+        $articles = $category->getArticles();
+
+        return $this->render('blog/category.html.twig', [
+            'category' => $category,
+            'articles' => $articles,
+        ]);
+    }
+///*========= Fin Quête 9 Symfony =========
+
+/* ===== Quête 7 Symfony =====
+
+    /**
+    * Getting a category
+    *
+    * @param string $category
+    *
+    * @Route("/blog/category/{category}",
+    *     defaults={"category" = null},
+    *     name="show_category")
+    * @return Response A response instance
+    */
+/*
+    public function showByCategory(Category $category): response
+    {
+        if (!$category) {
+            throw $this
+                ->createNotFoundException('No category has been sent to find an article in article\'s table.');
+    }
+
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneByName($category);
+
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findByCategory($category, ['id' => 'DESC'],3);
+
+        return $this->render('blog/category.html.twig',[
+                'category' => $category,
+                'articles' => $articles,
+        ]);
+    }
+===== Fin Quête 7 Symfony =====*/
+
+/*=====  Quête 8 Symfony =====
     /**
      * Getting a category
      *
@@ -99,6 +154,7 @@ class BlogController extends AbstractController
      *     name="show_category")
      * @return Response A response instance
      */
+/*
     public function showByCategory(string $category): response
     {
         $categoryObject = $this->getDoctrine()
@@ -112,28 +168,6 @@ class BlogController extends AbstractController
             'category' => $categoryObject,
             'articles' => $articles,
         ]);
-
-        /*
-        if (!$category) {
-            throw $this
-                ->createNotFoundException('No category has been sent to find an article in article\'s table.');
-        }
-
-        $category = $this->getDoctrine()
-            ->getRepository(Category::class)
-            ->findOneByName($category);
-
-        $articles = $this->getDoctrine()
-            ->getRepository(Article::class)
-            ->findByCategory($category, ['id' => 'DESC'],3);
-
-        return $this->render(
-            'blog/category.html.twig',
-            [
-                'category' => $category,
-                'articles' => $articles,
-            ]
-        );
-        */
     }
+//===== Fin Quête 8 Symfony =====*/
 }
