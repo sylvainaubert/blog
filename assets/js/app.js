79407c6ba36f9c1454e655b2e4bc7726b3efdaaf
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-05-21 13:21:25 +0200

    implement Bootstrap Jquery + default navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,17 @@
 require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+
