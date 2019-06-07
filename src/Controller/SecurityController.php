bffeca26665a73cadd78ef16d203a75c05f61349
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-06-07 21:21:22 +0200

    Add Logout function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,4 +24,12 @@ class SecurityController extends AbstractController
             'error' => $error
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout()
+    {
+        throw new \Exception('SUCCESS... In the logout...');
+    }
 }
