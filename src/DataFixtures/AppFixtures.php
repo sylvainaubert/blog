1634390b665becdd86622d670eacf0b8bb0f4a93
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-06-17 11:06:29 +0200

    Bug fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,61 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Tag;
+use App\Entity\Article;
+use App\Service\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class AppFixtures extends Fixture implements DependentFixtureInterface
 {
+    /**
+     * @var Slugify
+     */
+    private $slugify;
+
+    /**
+     * AppFixtures constructor.
+     * @param Slugify $slugify
+     */
+    public function __construct(Slugify $slugify)
+    {
+        $this->slugify = $slugify;
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [UserFixtures::class];
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 1000; $i++) {
+            $category = new Category();
+            $category->setName("category " . $i);
+            $manager->persist($category);
+
+            $tag = new Tag();
+            $tag->setName("tag " . $i);
+            $manager->persist($tag);
+
+            $article = new Article();
+            $article->setTitle("article " . $i);
+            $article->setSlug($this->slugify->generate($article->getTitle()));
+            $article->setContent("article " . $i . " content");
+            $article->setCategory($category);
+            $article->setAuthor($this->getReference('user_' . rand(0,1)));
+            $article->addTag($tag);
+            $manager->persist($article);
+        }
 
         $manager->flush();
     }
